f7c53b8a8f5d9bc43463715ad3da528770320015
vedashrith <vedashrithgoud@gmail.com>
2025-12-10 11:31:30 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "medical_chatbot",
+    version = "0.1.0",
+    author = "Vedashrith Survi",
+    author_email = "vedashrithgoud@gmail.com",
+    packages = find_packages(),
+    install_requires = []        
+)
